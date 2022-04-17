c23f244a85c4714968db753d8dcda2d19835d187
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-17 21:09:19 +0300

    grid and board

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from 'react';
 
 import './Board.css';
 
+const BOARD_SIZE = 10
+
 const Board = () => {
-    return <div>Foobar</div>
-};
+    const [board, setBoard] = useState(
+        new Array(BOARD_SIZE).fill(0).map(row => Array[BOARD_SIZE].fill(0))
+    );
+
+    return (
+        <div className="board">
+            {board.map((row, rowIdx) => (
+                <div key={rowIdx} className="row">{
+                    row.map((cell, cellIdx) => (
+                        <div key={cellIdx} className="cell"></div>
+                    ))
+                }</div>
+            ))}
+        </div>
+    )
+}
+
+
 
 export default Board;
