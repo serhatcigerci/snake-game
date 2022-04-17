e9232ecb6827d3d76f75db9191d2c8f19889bab9
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-17 21:20:46 +0300

    fixing index.js

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -6,7 +6,7 @@ const BOARD_SIZE = 10
 
 const Board = () => {
     const [board, setBoard] = useState(
-        new Array(BOARD_SIZE).fill(0).map(row => Array[BOARD_SIZE].fill(0))
+        new Array(BOARD_SIZE).fill(0).map(row => Array[BOARD_SIZE].fill(0)),
     );
 
     return (
