e01d0fc9a5431e02f2eefbb4fc22bfee651d3665
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-18 19:03:24 +0300

    food hardcode

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
 
+import { update as updateFood, draw as drawFood } from './food.js'
 
 let lastRenderTime = 0
 const gameBoard = document.getElementById('game-board')
